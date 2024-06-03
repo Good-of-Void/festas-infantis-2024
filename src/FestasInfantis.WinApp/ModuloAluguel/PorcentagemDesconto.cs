5f3ab455ead0b23162e2a7b66eb9c744348f40f1
Lucas Thiesen <lucasthiesen@LIVE.COM>
2024-06-03 17:40:46 -0300

    Modulos praticamente completos

---
+++ src/FestasInfantis.WinApp/ModuloAluguel/PorcentagemDesconto.cs
@@ -0,0 +1,32 @@
+﻿namespace FestasInfantis.WinApp.ModuloAluguel
+{
+    public class ConfiguracaoDesconto
+    {
+        //variáveis
+        public decimal PorcentagemDesconto { get; set; }
+        public decimal PorcentagemMaxima { get; set; }
+
+        //descontos
+        public ConfiguracaoDesconto()
+        {
+            PorcentagemDesconto = 1m;
+            PorcentagemMaxima = 15;
+        }
+
+        public ConfiguracaoDesconto(decimal porcentagemPorDesconto, decimal porcentagemMaxima)
+        {
+            PorcentagemDesconto = porcentagemPorDesconto;
+            PorcentagemMaxima = porcentagemMaxima;
+        }
+        //validando os descontos
+        public List<string> Validar()
+        {
+            List<string> erros = new List<string>();
+
+            if (PorcentagemDesconto > PorcentagemMaxima)
+                erros.Add("A porcentagem de desconto não pode ultrapassar a porcentagem máxima!");
+
+            return erros;
+        }
+    }
+}
---
+++ src/FestasInfantis.WinApp/ModuloItem/Item.cs
@@ -0,0 +1,47 @@
+﻿using FestasInfantis.WinApp.Compartilhado;
+namespace FestasInfantis.WinApp.ModuloItem
+{
+    //variaveis
+    public class Item : EntidadeBase
+    {
+        public string descricao;
+        public decimal valor;
+        //construtor
+        public Item(string descricao, decimal valor)
+        {
+            this.descricao = descricao;
+            this.valor = valor;
+        }
+        //atualizando informações
+        public override void AtualizarRegistro(EntidadeBase novoRegistro)
+        {
+            Item novo = (Item)novoRegistro;
+
+            this.Id = novo.Id;
+            this.descricao = novo.descricao;
+            this.valor = novo.valor;
+        }
+        //validando informações
+        public override List<String> Validar()
+        {
+            List<string> erros = new List<string>();
+
+            if (string.IsNullOrEmpty(descricao))
+                erros.Add("O campo 'Descrição' é obrigatório");
+
+            if (descricao.Length < 3)
+                erros.Add("O campo 'Descrição' deve conter no mínimo 3 caracteres");
+
+            if (valor < 1)
+                erros.Add("O campo 'Valor' não pode receber o valor 0");
+
+            return erros;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} Descrição: {descricao} Valor: {valor}";
+        }
+ 
+    }
+}
