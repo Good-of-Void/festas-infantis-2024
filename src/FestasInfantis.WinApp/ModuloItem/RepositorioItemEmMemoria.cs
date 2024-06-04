61dd03527e8c712528100b666c7cd12e933b9911
Caio Pagliosa <caio.pinipagli@gmail.com>
2024-06-04 15:54:59 -0300

    implementando modolo item

--- src/FestasInfantis.WinApp/ModuloItem/RepositorioItemEmMemoria.cs
+++ src/FestasInfantis.WinApp/ModuloItem/RepositorioItemEmMemoria.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FestasInfantis.WinApp.Compartilhado;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FestasInfantis.WinApp.ModuloItem
 {
-    internal class RepositorioItem
+    internal class RepositorioItemEmMemoria : RepositorioBase<Item> , 
     {
     }
 }
