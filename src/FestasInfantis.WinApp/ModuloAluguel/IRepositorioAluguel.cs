b85d1757dd440c807d32bda47459a34d7f421dfd
Lucas Thiesen <lucasthiesen@LIVE.COM>
2024-06-05 13:38:37 -0300

    Irepositorio clientes e aluguel

--- src/FestasInfantis.WinApp/ModuloAluguel/IRepositorioAluguel.cs
+++ src/FestasInfantis.WinApp/ModuloAluguel/IRepositorioAluguel.cs
@@ -8,5 +8,10 @@ namespace FestasInfantis.WinApp.ModuloAluguel
 {
     public interface IRepositorioAluguel
     {
+        void Cadastrar(Aluguel novoAluguel);
+        bool Editar(int id, Aluguel aluguelEditado);
+        bool Excluir(int id);
+        Aluguel SelecionarPorId(int idSelecionado);
+        List<Aluguel> SelecionarTodos();
     }
 }
--- src/FestasInfantis.WinApp/ModuloCliente/IRepositorioCliente.cs
+++ src/FestasInfantis.WinApp/ModuloCliente/IRepositorioCliente.cs
@@ -8,5 +8,10 @@ namespace FestasInfantis.WinApp.ModuloCliente
 {
     internal interface IRepositorioCliente
     {
+        void Cadastrar(Cliente novoCliente);
+        bool Editar(int id, Cliente clienteEditado);
+        bool Excluir(int id);
+        Cliente SelecionarPorId(int idSelecionado);
+        List<Cliente> SelecionarTodos();
     }
 }
