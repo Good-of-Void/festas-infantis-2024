1311a48d97ae3837660596cdab07877ad6f0d86b
Caio Pagliosa <caio.pinipagli@gmail.com>
2024-06-06 14:13:56 -0300

    tela aluguel

--- src/FestasInfantis.WinApp/ModuloAluguel/TelaAluguelForm.Designer.cs
+++ src/FestasInfantis.WinApp/ModuloAluguel/TelaAluguelForm.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            Painel = new Panel();
             brnGravar = new Button();
             btnCancelar = new Button();
             txtId = new TextBox();
-            label1 = new Label();
             lblTitulo = new Label();
-            Painel = new Panel();
+            label1 = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -47,21 +47,27 @@
             panel1.Controls.Add(txtId);
             panel1.Controls.Add(lblTitulo);
             panel1.Controls.Add(label1);
-            panel1.Location = new Point(12, 11);
-            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Location = new Point(14, 15);
             panel1.Name = "panel1";
-            panel1.Size = new Size(625, 501);
+            panel1.Size = new Size(799, 726);
             panel1.TabIndex = 1;
+            // 
+            // Painel
+            // 
+            Painel.Location = new Point(14, 135);
+            Painel.Margin = new Padding(3, 4, 3, 4);
+            Painel.Name = "Painel";
+            Painel.Size = new Size(769, 514);
+            Painel.TabIndex = 11;
             // 
             // brnGravar
             // 
             brnGravar.BackColor = Color.FromArgb(249, 247, 247);
             brnGravar.Font = new Font("Verdana", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             brnGravar.ForeColor = Color.FromArgb(63, 114, 175);
-            brnGravar.Location = new Point(348, 446);
-            brnGravar.Margin = new Padding(3, 2, 3, 2);
+            brnGravar.Location = new Point(486, 656);
             brnGravar.Name = "brnGravar";
-            brnGravar.Size = new Size(118, 41);
+            brnGravar.Size = new Size(135, 55);
             brnGravar.TabIndex = 10;
             brnGravar.Text = "Gravar";
             brnGravar.UseVisualStyleBackColor = false;
@@ -71,10 +77,9 @@
             btnCancelar.BackColor = Color.FromArgb(249, 247, 247);
             btnCancelar.Font = new Font("Verdana", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnCancelar.ForeColor = Color.FromArgb(63, 114, 175);
-            btnCancelar.Location = new Point(488, 446);
-            btnCancelar.Margin = new Padding(3, 2, 3, 2);
+            btnCancelar.Location = new Point(648, 656);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(118, 41);
+            btnCancelar.Size = new Size(135, 55);
             btnCancelar.TabIndex = 10;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = false;
@@ -85,50 +90,41 @@
             txtId.Enabled = false;
             txtId.Font = new Font("Verdana", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
             txtId.ForeColor = Color.FromArgb(63, 114, 175);
-            txtId.Location = new Point(70, 18);
-            txtId.Margin = new Padding(3, 2, 3, 2);
+            txtId.Location = new Point(80, 24);
             txtId.Name = "txtId";
-            txtId.Size = new Size(52, 30);
+            txtId.Size = new Size(59, 35);
             txtId.TabIndex = 9;
             txtId.Text = "0";
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.Font = new Font("Verdana", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitulo.ForeColor = Color.FromArgb(63, 114, 175);
+            lblTitulo.Location = new Point(14, 94);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(769, 37);
+            lblTitulo.TabIndex = 0;
+            lblTitulo.Text = "Título";
+            lblTitulo.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Verdana", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.FromArgb(63, 114, 175);
-            label1.Location = new Point(31, 21);
+            label1.Location = new Point(35, 28);
             label1.Name = "label1";
-            label1.Size = new Size(33, 23);
+            label1.Size = new Size(41, 28);
             label1.TabIndex = 0;
             label1.Text = "Id";
             // 
-            // lblTitulo
-            // 
-            lblTitulo.AutoSize = true;
-            lblTitulo.Font = new Font("Verdana", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblTitulo.ForeColor = Color.FromArgb(63, 114, 175);
-            lblTitulo.Location = new Point(31, 66);
-            lblTitulo.Name = "lblTitulo";
-            lblTitulo.Size = new Size(73, 23);
-            lblTitulo.TabIndex = 0;
-            lblTitulo.Text = "Título";
-            // 
-            // Painel
-            // 
-            Painel.Location = new Point(31, 101);
-            Painel.Name = "Painel";
-            Painel.Size = new Size(562, 323);
-            Painel.TabIndex = 11;
-            // 
             // TelaAluguelForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(17, 45, 78);
-            ClientSize = new Size(649, 523);
+            ClientSize = new Size(825, 753);
             Controls.Add(panel1);
-            Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "TelaAluguelForm";
